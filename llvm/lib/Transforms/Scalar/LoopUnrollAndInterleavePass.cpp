5ccb84eb4fe76bfadcecf1740eb430d18bda2917
Ivan Radanov Ivanov <ivanov2@llnl.gov>
2023-10-26 14:01:34 -0700

    comment

--- llvm/lib/Transforms/Scalar/LoopUnrollAndInterleavePass.cpp
+++ llvm/lib/Transforms/Scalar/LoopUnrollAndInterleavePass.cpp
@@ -1,5 +1,4 @@
-//===- LoopUnrollAndInterleavePass.cpp - Loop unroller pass
-//--------------------------------===//
+//===- LoopUnrollAndInterleavePass.cpp - Loop unroller pass----------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
