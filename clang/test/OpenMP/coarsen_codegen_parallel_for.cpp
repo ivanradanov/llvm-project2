218eb0d9fc74609644b454fd65f0c68b238c734d
Ivan Radanov Ivanov <ivanov2@llnl.gov>
2023-12-22 17:22:41 -0800

    Add parallel for coarsen codegen

---
+++ clang/test/OpenMP/coarsen_codegen_parallel_for.cpp
@@ -0,0 +1,26 @@
+// RUN: %clang_cc1 -verify -triple x86_64-pc-linux-gnu -fopenmp -emit-llvm %s -o - | FileCheck %s --check-prefix=IR
+
+// Check same results after serialization round-trip
+// RUN: %clang_cc1 -verify -triple x86_64-pc-linux-gnu -fopenmp -emit-pch -o %t %s
+// RUN: %clang_cc1 -verify -triple x86_64-pc-linux-gnu -fopenmp -include-pch %t -emit-llvm %s -o - | FileCheck %s --check-prefix=IR
+// expected-no-diagnostics
+
+#ifndef HEADER
+#define HEADER
+
+// placeholder for loop body code.
+extern "C" void body(...) {}
+
+extern "C" void func(int start, int end, int step) {
+#pragma omp parallel for ompx_coarsen_for(2)
+  for (int i = start; i < end; i+=step)
+    body(start, end, step, i);
+}
+
+#endif
+
+
+// IR-LABEL:  @func(
+// IR:  br label %{{.*}} !llvm.loop ![[LOOP:.+]]
+// IR:  ![[LOOP]] = distinct{{.*}}![[LOOP1:[0-9]+]]
+// IR:  ![[LOOP1]] = !{!"llvm.loop.unroll_and_interleave.count", i32 2}
