b626d95b14461f6a3fcd6feffd8347dea62ec1b2
Ivan Radanov Ivanov <ivanov2@llnl.gov>
2024-01-23 18:34:04 -0800

    Fix omp runtime assign

--- flang/omp_runtime/assign.cpp
+++ flang/omp_runtime/assign.cpp
@@ -1,5 +1,5 @@
-#include "../runtime/terminator.h"
 #include "../runtime/freestanding-tools.h"
+#include "../runtime/terminator.h"
 #include "../runtime/type-info.h"
 #include "flang/Runtime/descriptor.h"
 #include "flang/Runtime/entry-names.h"
@@ -19,34 +19,41 @@ enum AssignFlags {
   DeallocateLHS = 1 << 6
 };
 
-
-RT_API_ATTRS static void Assign(
-    Descriptor &to, const Descriptor &from, Terminator &terminator, int flags, int32_t omp_device) {
+RT_API_ATTRS static void Assign(Descriptor &to, const Descriptor &from,
+    Terminator &terminator, int flags, int32_t omp_device) {
   std::size_t toElementBytes{to.ElementBytes()};
   std::size_t fromElementBytes{from.ElementBytes()};
   std::size_t toElements{to.Elements()};
   std::size_t fromElements{from.Elements()};
 
   if (toElementBytes != fromElementBytes)
-    terminator.Crash("Assign: unhandled case");
+    terminator.Crash("Assign: toElementBytes != fromElementBytes");
   if (toElements != fromElements)
-    terminator.Crash("Assign: unhandled case");
+    terminator.Crash("Assign: toElements != fromElements");
 
   void *host_to_ptr = to.raw().base_addr;
   void *host_from_ptr = from.raw().base_addr;
   size_t length = toElements * toElementBytes;
 
-  if (omp_target_is_present(host_to_ptr, omp_device))
-    terminator.Crash("Assign: unhandled case");
-  if (omp_target_is_present(host_from_ptr, omp_device))
-    terminator.Crash("Assign: unhandled case");
+  printf("assign length: %zu\n", length);
 
-  #pragma omp target data map(to: host_to_ptr) map(to: host_to_ptr)
-  omp_target_memcpy(host_to_ptr, host_from_ptr, length, 0, 0, omp_device, omp_device);
+  if (!omp_target_is_present(host_to_ptr, omp_device))
+    terminator.Crash("Assign: !omp_target_is_present(host_to_ptr, omp_device)");
+  if (!omp_target_is_present(host_from_ptr, omp_device))
+    terminator.Crash(
+        "Assign: !omp_target_is_present(host_from_ptr, omp_device)");
+
+  printf("host_to_ptr: %p\n", host_to_ptr);
+#pragma omp target data use_device_ptr(host_to_ptr, host_from_ptr) device(omp_device)
+  {
+    printf("device_to_ptr: %p\n", host_to_ptr);
+    // TODO do we need to handle overlapping memory? does this function do that?
+    omp_target_memcpy(host_to_ptr, host_from_ptr, length, /*dst_offset*/ 0,
+        /*src_offset*/ 0, /*dst*/ omp_device, /*src*/ omp_device);
+  }
 
   return;
 }
-
 
 extern "C" {
 RT_EXT_API_GROUP_BEGIN
@@ -58,7 +65,8 @@ void RTDEF(Assign_omp)(Descriptor &to, const Descriptor &from,
   // will have been already been converted to calls, so don't check for
   // defined assignment apart from components.
   Assign(to, from, terminator,
-      MaybeReallocate | NeedFinalization | ComponentCanBeDefinedAssignment, omp_device);
+      MaybeReallocate | NeedFinalization | ComponentCanBeDefinedAssignment,
+      omp_device);
 }
 
 } // extern "C"
