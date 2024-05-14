48a1cfa8255946d2e5dc15519d0d8ef612db8f6a
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-05-14 10:31:30 +0900

    Add copies

--- memred-runtimes/trace.cpp
+++ memred-runtimes/trace.cpp
@@ -13,7 +13,7 @@
 static void checkErr(cudaError_t Err, const char *File, int Line) {
   if (Err != cudaSuccess) {
     fprintf(stderr, "%s:%d: %s\n", File, Line, cudaGetErrorString(Err));
-    // abort();
+    abort();
   }
 }
 
@@ -65,27 +65,94 @@ struct ObjectAddressing {
   }
 };
 
+#define DUMP(X)                                                                \
+  do {                                                                         \
+    Log << " " #X " " << X;                                                    \
+  } while (0)
+
 static struct EventsTy {
   class EventTy {
   public:
-    size_t Idx = -1;
+    virtual void dump(std::ostream &Log) { Log << "UNKNOWN"; }
     virtual ~EventTy() = default;
+  };
+  class KernelCallTy : public EventTy {
+  public:
+    const char *Name;
+    cudaStream_t Stream;
+    void dump(std::ostream &Log) override {
+      Log << "Kernel call:";
+      DUMP(Name);
+      DUMP(Stream);
+    }
+  };
+  class CopyTy : public EventTy {
+  public:
+    enum cudaMemcpyKind Kind = cudaMemcpyDefault;
+    cudaStream_t Stream = 0;
+    const void *From = nullptr;
+    void *To = nullptr;
+    size_t Size = 0;
+    bool Async = false;
+    void dump(std::ostream &Log) override {
+      Log << "Copy:";
+      DUMP(Kind);
+      DUMP(Stream);
+      DUMP(From);
+      DUMP(To);
+      DUMP(Size);
+    }
   };
   class AllocationTy : public EventTy {
   public:
+    size_t Idx = 0;
     void *RealPtr = nullptr;
     void *VirtualPtr = nullptr;
     size_t Size = 0;
+    void dump(std::ostream &Log) override {
+      Log << "Allocation:";
+      DUMP(Idx);
+      DUMP(RealPtr);
+      DUMP(VirtualPtr);
+      DUMP(Size);
+    }
   };
+
   std::list<std::unique_ptr<EventTy>> Events;
+  size_t NumAllocations = 0;
+
+  KernelCallTy *insertNewKernelCall(const char *Name, void **Args,
+                                    cudaStream_t Stream) {
+    // TODO figure out the pointer args
+    auto K = std::make_unique<KernelCallTy>();
+    K->Name = Name;
+    auto *Ret = K.get();
+    Events.push_back(std::move(K));
+    return Ret;
+  }
+
+  CopyTy *insertNewCopy(const void *From, void *To, size_t Size,
+                        enum cudaMemcpyKind Kind, cudaStream_t Stream,
+                        bool Async) {
+    auto C = std::make_unique<CopyTy>();
+    C->From = From;
+    C->To = To;
+    C->Size = Size;
+    C->Kind = Kind;
+    C->Stream = Stream;
+    C->Async = Async;
+    auto *Ret = C.get();
+    Events.push_back(std::move(C));
+    return Ret;
+  }
 
   AllocationTy *insertNewAllocation(void *RealPtr, size_t Size) {
-    size_t Idx = Events.size();
+    size_t Idx = NumAllocations;
     auto A = std::make_unique<AllocationTy>();
-    A->Idx = Idx;
     A->RealPtr = RealPtr;
     A->VirtualPtr = OA.localPtrToGlobalPtr(Idx, OA.getObjBasePtr());
     A->Size = Size;
+    A->Idx = Idx;
     auto *Ret = A.get();
     Events.push_back(std::move(A));
     return Ret;
@@ -98,16 +165,17 @@ static struct EventsTy {
   ~EventsTy() {
     auto &Log = std::cerr;
     Log << "Graph:\n";
+    size_t Idx = 0;
     for (auto &Event : Events) {
-      if (auto *A = dynamic_cast<EventsTy::AllocationTy *>(&*Event))
-        Log << A->Idx << ": "
-            << "Size " << A->Size << " Real " << A->RealPtr << " Virtual "
-            << A->VirtualPtr << "\n";
+      Log << Idx << ": ";
+      Event->dump(Log);
+      Log << "\n";
+      Idx++;
     }
   }
 
   ObjectAddressing OA;
-} Allocations;
+} Events;
 
 #define MEMRED_ATTRS extern "C"
 
@@ -115,10 +183,10 @@ MEMRED_ATTRS cudaError_t __memred_cudaMalloc(void **p, size_t s) {
   void *Ptr;
   cudaError_t Err = cudaMalloc(&Ptr, s);
   CHECK_ERR(Err);
-  auto A = Allocations.insertNewAllocation(Ptr, s);
+  auto *A = Events.insertNewAllocation(Ptr, s);
   // TODO should return virtual ptr
-  *p = Ptr;
   // *p = A.VirtualPtr;
+  *p = Ptr;
   return Err;
 }
 
@@ -128,10 +196,13 @@ MEMRED_ATTRS cudaError_t __memred_cudaLaunchKernel(const void *func,
                                                    size_t sharedMem,
                                                    cudaStream_t stream) {
   // TODO need to translate addresses here
-  // abort();
   cudaError_t Err =
       cudaLaunchKernel(func, gridDim, blockDim, args, sharedMem, stream);
   CHECK_ERR(Err);
+
+  const char *Name;
+  CHECK_ERR(cudaFuncGetName(&Name, func));
+  Events.insertNewKernelCall(Name, args, stream);
   return Err;
 }
 
@@ -139,19 +210,17 @@ MEMRED_ATTRS cudaError_t __memred_cudaMemcpyAsync(void *dst, const void *src,
                                                   size_t count,
                                                   enum cudaMemcpyKind kind,
                                                   cudaStream_t stream) {
-  // TODO
-  // abort();
   cudaError_t Err = cudaMemcpyAsync(dst, src, count, kind, stream);
   CHECK_ERR(Err);
+  Events.insertNewCopy(src, dst, count, kind, stream, true);
   return Err;
 }
 
 MEMRED_ATTRS cudaError_t __memred_cudaMemcpy(void *dst, const void *src,
                                              size_t count,
                                              enum cudaMemcpyKind kind) {
-  // TODO
-  // abort();
   cudaError_t Err = cudaMemcpy(dst, src, count, kind);
   CHECK_ERR(Err);
+  Events.insertNewCopy(src, dst, count, kind, 0, false);
   return Err;
 }
