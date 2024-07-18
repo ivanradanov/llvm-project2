431068675aaf830459a6af0920812e0bc144d80c
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-07-18 16:49:36 +0900

    Remove unused code

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -1245,20 +1245,6 @@ void EmitAssemblyHelper::RunTransformer() {
     llvm::errs() << "Mlir passes failed";
     abort();
   }
-
-  constexpr char TransformSequence[] = R"RAWSTRING(
-transform.sequence %arg0: !transform.any_op {} {
-  transform.loop.unroll %arg0 { factor = 4 } : !transform.any_op
-  transform.yield
-}
-)RAWSTRING";
-
-  // auto Seq = mlir::parseSourceString(TransformSequence);
-  // llvm::errs() << Seq << "\n"
-  // MlirModule->walk([&](scf::ForOp forOp) {
-  //   mlir::transform::applyTransformSequence(forOp,MlirModule);
-  // });
-
   llvm::errs() << "Post-transform MLIR\n" << *MlirModule << "\n";
 }
 
