429cb984af610e93e18e333cb89081c6fb002975
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-07-22 10:59:25 +0900

    Change up pragma test file

---
+++ clang/test/CodeGen/pragma-transform.cpp
@@ -0,0 +1,21 @@
+// RUN: %clang_cc1 -mllvm --transformer-enable -triple x86_64-pc-linux-gnu -emit-llvm %s -o - | FileCheck %s
+
+
+void foo(float *a, float *b, int n) {
+
+#pragma transform label for1
+  for (int i = 0; i < n; i++) {
+    a[i] = b[i] * 2;
+  }
+
+#pragma transform import
+R"TRANSFORM(
+transform.named_sequence @transform1(%arg0: !transform.any_op) {
+  transform.loop.unroll %arg0 { factor = 4 } : !transform.any_op
+  transform.yield
+}
+)TRANSFORM";
+
+#pragma transform apply transform1(for1)
+
+}
