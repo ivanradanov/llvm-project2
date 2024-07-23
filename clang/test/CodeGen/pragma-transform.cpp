86398bbeefc1fc2eff54241e9ddb1f9109d7395a
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-07-23 14:29:56 +0900

    Change the test

--- clang/test/CodeGen/pragma-transform.cpp
+++ clang/test/CodeGen/pragma-transform.cpp
@@ -1,22 +1,25 @@
 // RUN: %clang_cc1 -mllvm --transformer-enable -triple x86_64-pc-linux-gnu -emit-llvm %s -o - | FileCheck %s
 
 
-void foo(float *a, float *b, int n) {
+void foo(float *a, float *b, int n, int k) {
 
-  #pragma transform_label for2
-  for (int i = 0; i < n; i++) {
-    a[i] = b[i] * 2;
+  #pragma transform_label for1
+  for (int j = 0; j < n; j++) {
+    #pragma transform_label for2
+    for (int i = 0; i < n; i++) {
+      a[i + j * k] = b[i + j * k] * 2;
+    }
   }
 
   #pragma transform_import
   R"TRANSFORM(
-  transform.named_sequence @transform2(%arg0: !transform.any_op) {
+  transform.named_sequence @transform2(%arg0: !transform.any_op, %arg0: !transform.any_op) {
     transform.loop.unroll %arg0 { factor = 2 } : !transform.any_op
     transform.yield
   }
   )TRANSFORM";
 
-  #pragma transform_apply transform1(for2)
+  #pragma transform_apply transform2(for2, for4)
 
   #pragma transform_label another_for
   for (int i = 0; i < n; i++) {
