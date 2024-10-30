496e0c9892c9567114a43dc51750f987be843a8c
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-10-30 11:50:11 +0900

    WIP Relay array expansion info to the ast codegen

--- polly/lib/External/isl/include/isl/schedule_node.h
+++ polly/lib/External/isl/include/isl/schedule_node.h
@@ -15,6 +15,9 @@
 extern "C" {
 #endif
 
+__isl_export __isl_give isl_union_set *
+isl_schedule_node_domain_get_expanded_arrays(
+	__isl_keep isl_schedule_node *node);
 __isl_export __isl_give isl_schedule_node *
 isl_schedule_node_domain_set_expanded_arrays(
 	__isl_take isl_schedule_node *node,
