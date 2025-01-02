1a2d43bec4fd361550e82b941ac3fdf734b6514f
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-01-02 19:11:37 +0900

    Prepare for proper minimize using ast import

--- clang/tools/inputgen-minimize/InputGenMinimize.cpp
+++ clang/tools/inputgen-minimize/InputGenMinimize.cpp
@@ -18,14 +18,19 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Decl.h"
+#include "clang/AST/ExternalASTMerger.h"
 #include "clang/AST/ParentMapContext.h"
 #include "clang/AST/PrettyPrinter.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/Type.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Driver/Options.h"
+#include "clang/Driver/Types.h"
 #include "clang/Frontend/ASTConsumers.h"
 #include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/MultiplexConsumer.h"
+#include "clang/Frontend/TextDiagnosticBuffer.h"
+#include "clang/Parse/ParseAST.h"
 #include "clang/Rewrite/Frontend/FixItRewriter.h"
 #include "clang/Rewrite/Frontend/FrontendActions.h"
 #include "clang/StaticAnalyzer/Frontend/FrontendActions.h"
@@ -41,6 +46,7 @@
 #include "llvm/Support/Signals.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/TargetParser/Host.h"
 
 using namespace clang::driver;
 using namespace clang::tooling;
@@ -231,8 +237,7 @@ class ASTMinimizer : public ASTConsumer,
   bool Done = false;
 
 public:
-  ASTMinimizer(std::unique_ptr<raw_ostream> Out)
-      : Out(Out ? *Out : llvm::outs()), OwnedOut(std::move(Out)) {}
+  ASTMinimizer(raw_ostream &Out) : Out(Out) {}
 
   TranslationUnitDecl *TUD;
   void HandleTranslationUnit(ASTContext &Context) override {
@@ -292,11 +297,13 @@ public:
                        << FD->getName() << "\n";
           return true;
         }
+#if 0
         Out << "\n";
         Out << "// =========== RECORDED FUNCTION BEGIN ===========\n";
         PrintWithDeps(D);
         Out << "// =========== RECORDED FUNCTION END ===========\n";
         Out << "\n";
+#endif
         Out << "// =========== GENERATED ENTRY POINT BEGIN ===========\n";
         generateEntryPoint(FD);
         Out << "// =========== GENERATED ENTRY POINT END ===========\n";
@@ -321,18 +328,272 @@ private:
   }
 
   raw_ostream &Out;
-  std::unique_ptr<raw_ostream> OwnedOut;
 };
 
-std::unique_ptr<ASTConsumer>
-CreateASTMinimizer(std::unique_ptr<raw_ostream> Out) {
-  return std::make_unique<ASTMinimizer>(std::move(Out));
+std::unique_ptr<ASTConsumer> CreateASTMinimizer(raw_ostream &Out) {
+  return std::make_unique<ASTMinimizer>(Out);
 }
 
-class MimimizeFactory {
+namespace init_convenience {
+class TestDiagnosticConsumer : public DiagnosticConsumer {
+private:
+  std::unique_ptr<TextDiagnosticBuffer> Passthrough;
+  const LangOptions *LangOpts = nullptr;
+
 public:
+  TestDiagnosticConsumer()
+      : Passthrough(std::make_unique<TextDiagnosticBuffer>()) {}
+
+  void BeginSourceFile(const LangOptions &LangOpts,
+                       const Preprocessor *PP = nullptr) override {
+    this->LangOpts = &LangOpts;
+    return Passthrough->BeginSourceFile(LangOpts, PP);
+  }
+
+  void EndSourceFile() override {
+    this->LangOpts = nullptr;
+    Passthrough->EndSourceFile();
+  }
+
+  bool IncludeInDiagnosticCounts() const override {
+    return Passthrough->IncludeInDiagnosticCounts();
+  }
+
+private:
+  static void PrintSourceForLocation(const SourceLocation &Loc,
+                                     SourceManager &SM) {
+    const char *LocData = SM.getCharacterData(Loc, /*Invalid=*/nullptr);
+    unsigned LocColumn =
+        SM.getSpellingColumnNumber(Loc, /*Invalid=*/nullptr) - 1;
+    FileID FID = SM.getFileID(Loc);
+    llvm::MemoryBufferRef Buffer = SM.getBufferOrFake(FID, Loc);
+
+    assert(LocData >= Buffer.getBufferStart() &&
+           LocData < Buffer.getBufferEnd());
+
+    const char *LineBegin = LocData - LocColumn;
+
+    assert(LineBegin >= Buffer.getBufferStart());
+
+    const char *LineEnd = nullptr;
+
+    for (LineEnd = LineBegin; *LineEnd != '\n' && *LineEnd != '\r' &&
+                              LineEnd < Buffer.getBufferEnd();
+         ++LineEnd)
+      ;
+
+    llvm::StringRef LineString(LineBegin, LineEnd - LineBegin);
+
+    llvm::errs() << LineString << '\n';
+    llvm::errs().indent(LocColumn);
+    llvm::errs() << '^';
+    llvm::errs() << '\n';
+  }
+
+  void HandleDiagnostic(DiagnosticsEngine::Level DiagLevel,
+                        const Diagnostic &Info) override {
+    if (Info.hasSourceManager() && LangOpts) {
+      SourceManager &SM = Info.getSourceManager();
+
+      if (Info.getLocation().isValid()) {
+        Info.getLocation().print(llvm::errs(), SM);
+        llvm::errs() << ": ";
+      }
+
+      SmallString<16> DiagText;
+      Info.FormatDiagnostic(DiagText);
+      llvm::errs() << DiagText << '\n';
+
+      if (Info.getLocation().isValid()) {
+        PrintSourceForLocation(Info.getLocation(), SM);
+      }
+
+      for (const CharSourceRange &Range : Info.getRanges()) {
+        bool Invalid = true;
+        StringRef Ref = Lexer::getSourceText(Range, SM, *LangOpts, &Invalid);
+        if (!Invalid) {
+          llvm::errs() << Ref << '\n';
+        }
+      }
+    }
+    DiagnosticConsumer::HandleDiagnostic(DiagLevel, Info);
+  }
+};
+
+static llvm::cl::list<std::string>
+    ClangArgs("Xcc",
+              llvm::cl::desc("Argument to pass to the CompilerInvocation"),
+              llvm::cl::CommaSeparated);
+
+std::unique_ptr<CompilerInstance> BuildCompilerInstance() {
+  auto Ins = std::make_unique<CompilerInstance>();
+  auto DC = std::make_unique<TestDiagnosticConsumer>();
+  const bool ShouldOwnClient = true;
+  Ins->createDiagnostics(*llvm::vfs::getRealFileSystem(), DC.release(),
+                         ShouldOwnClient);
+
+  auto Inv = std::make_unique<CompilerInvocation>();
+
+  std::vector<const char *> ClangArgv(ClangArgs.size());
+  std::transform(ClangArgs.begin(), ClangArgs.end(), ClangArgv.begin(),
+                 [](const std::string &s) -> const char * { return s.data(); });
+  CompilerInvocation::CreateFromArgs(*Inv, ClangArgv, Ins->getDiagnostics());
+
+  std::string Input = "c++";
+  {
+    using namespace driver::types;
+    ID Id = lookupTypeForTypeSpecifier(Input.c_str());
+    assert(Id != TY_INVALID);
+    if (isCXX(Id)) {
+      Inv->getLangOpts().CPlusPlus = true;
+      Inv->getLangOpts().CPlusPlus11 = true;
+      Inv->getHeaderSearchOpts().UseLibcxx = true;
+    }
+    if (isObjC(Id)) {
+      Inv->getLangOpts().ObjC = 1;
+    }
+  }
+  Inv->getLangOpts().ObjCAutoRefCount = false;
+
+  // TODO we should collect these from the compilation for the file that
+  // contains the entry function
+  Inv->getLangOpts().Bool = true;
+  Inv->getLangOpts().WChar = true;
+  Inv->getLangOpts().Blocks = true;
+  Inv->getLangOpts().DebuggerSupport = true;
+  Inv->getLangOpts().SpellChecking = false;
+  Inv->getLangOpts().ThreadsafeStatics = false;
+  Inv->getLangOpts().AccessControl = false;
+  Inv->getLangOpts().DollarIdents = true;
+  Inv->getLangOpts().Exceptions = true;
+  Inv->getLangOpts().CXXExceptions = true;
+  // Needed for testing dynamic_cast.
+  Inv->getLangOpts().RTTI = true;
+  Inv->getCodeGenOpts().setDebugInfo(llvm::codegenoptions::FullDebugInfo);
+  Inv->getTargetOpts().Triple = llvm::sys::getDefaultTargetTriple();
+
+  Ins->setInvocation(std::move(Inv));
+
+  TargetInfo *TI = TargetInfo::CreateTargetInfo(
+      Ins->getDiagnostics(), Ins->getInvocation().TargetOpts);
+  Ins->setTarget(TI);
+  Ins->getTarget().adjust(Ins->getDiagnostics(), Ins->getLangOpts());
+  Ins->createFileManager();
+  Ins->createSourceManager(Ins->getFileManager());
+  Ins->createPreprocessor(TU_Complete);
+
+  return Ins;
+}
+
+std::unique_ptr<ASTContext>
+BuildASTContext(CompilerInstance &CI, SelectorTable &ST, Builtin::Context &BC) {
+  auto &PP = CI.getPreprocessor();
+  auto AST =
+      std::make_unique<ASTContext>(CI.getLangOpts(), CI.getSourceManager(),
+                                   PP.getIdentifierTable(), ST, BC, PP.TUKind);
+  AST->InitBuiltinTypes(CI.getTarget());
+  return AST;
+}
+
+} // namespace init_convenience
+
+/// A container for a CompilerInstance (possibly with an ExternalASTMerger
+/// attached to its ASTContext).
+///
+/// Provides an accessor for the DeclContext origins associated with the
+/// ExternalASTMerger (or an empty list of origins if no ExternalASTMerger is
+/// attached).
+///
+/// This is the main unit of parsed source code maintained by clang-import-test.
+struct CIAndOrigins {
+  using OriginMap = clang::ExternalASTMerger::OriginMap;
+  std::unique_ptr<CompilerInstance> CI;
+
+  ASTContext &getASTContext() { return CI->getASTContext(); }
+  FileManager &getFileManager() { return CI->getFileManager(); }
+  const OriginMap &getOriginMap() {
+    static const OriginMap EmptyOriginMap{};
+    if (ExternalASTSource *Source = CI->getASTContext().getExternalSource())
+      return static_cast<ExternalASTMerger *>(Source)->GetOrigins();
+    return EmptyOriginMap;
+  }
+  DiagnosticConsumer &getDiagnosticClient() {
+    return CI->getDiagnosticClient();
+  }
+  CompilerInstance &getCompilerInstance() { return *CI; }
+};
+
+void AddExternalSource(CIAndOrigins &CI,
+                       llvm::MutableArrayRef<CIAndOrigins> Imports) {
+  ExternalASTMerger::ImporterTarget Target(
+      {CI.getASTContext(), CI.getFileManager()});
+  llvm::SmallVector<ExternalASTMerger::ImporterSource, 3> Sources;
+  for (CIAndOrigins &Import : Imports)
+    Sources.emplace_back(Import.getASTContext(), Import.getFileManager(),
+                         Import.getOriginMap());
+  auto ES = std::make_unique<ExternalASTMerger>(Target, Sources);
+  CI.getASTContext().setExternalSource(ES.release());
+  CI.getASTContext().getTranslationUnitDecl()->setHasExternalVisibleStorage();
+}
+
+llvm::Error ParseSource(const std::string &Path, CompilerInstance &CI,
+                        ASTConsumer &Consumer) {
+  SourceManager &SM = CI.getSourceManager();
+  auto FE = CI.getFileManager().getFileRef(Path);
+  if (!FE) {
+    llvm::consumeError(FE.takeError());
+    return llvm::make_error<llvm::StringError>(
+        llvm::Twine("No such file or directory: ", Path), std::error_code());
+  }
+  SM.setMainFileID(SM.createFileID(*FE, SourceLocation(), SrcMgr::C_User));
+  ParseAST(CI.getPreprocessor(), &Consumer, CI.getASTContext());
+  return llvm::Error::success();
+}
+
+llvm::Expected<CIAndOrigins> Parse(const std::string &Path,
+                                   llvm::MutableArrayRef<CIAndOrigins> Imports,
+                                   bool ShouldDumpAST, bool ShouldDumpIR) {
+  CIAndOrigins CI{init_convenience::BuildCompilerInstance()};
+  auto ST = std::make_unique<SelectorTable>();
+  auto BC = std::make_unique<Builtin::Context>();
+  std::unique_ptr<ASTContext> AST =
+      init_convenience::BuildASTContext(CI.getCompilerInstance(), *ST, *BC);
+  CI.getCompilerInstance().setASTContext(AST.release());
+  if (Imports.size())
+    AddExternalSource(CI, Imports);
+
+  std::vector<std::unique_ptr<ASTConsumer>> ASTConsumers;
+
+  if (ShouldDumpAST)
+    ASTConsumers.push_back(CreateASTDumper(nullptr /*Dump to stdout.*/, "",
+                                           true, false, false, false,
+                                           clang::ADOF_Default));
+
+  CI.getDiagnosticClient().BeginSourceFile(
+      CI.getCompilerInstance().getLangOpts(),
+      &CI.getCompilerInstance().getPreprocessor());
+  MultiplexConsumer Consumers(std::move(ASTConsumers));
+  Consumers.Initialize(CI.getASTContext());
+
+  if (llvm::Error PE = ParseSource(Path, CI.getCompilerInstance(), Consumers))
+    return std::move(PE);
+  CI.getDiagnosticClient().EndSourceFile();
+  if (CI.getDiagnosticClient().getNumErrors())
+    return llvm::make_error<llvm::StringError>(
+        "Errors occurred while parsing the expression.", std::error_code());
+  return std::move(CI);
+}
+
+class MinimizeActionFactory {
+private:
+  std::string OutStr;
+  llvm::raw_string_ostream Out;
+
+public:
+  const std::string &getGeneratedEntry() { return Out.str(); };
+  MinimizeActionFactory() : Out(OutStr) {}
   std::unique_ptr<clang::ASTConsumer> newASTConsumer() {
-    return CreateASTMinimizer(nullptr);
+    return CreateASTMinimizer(Out);
   }
 };
 
@@ -354,13 +615,30 @@ int main(int argc, const char **argv) {
     return 1;
   }
   CommonOptionsParser &OptionsParser = ExpectedParser.get();
+
+  std::vector<CIAndOrigins> ImportCIs;
+  for (auto I : OptionsParser.getSourcePathList()) {
+    llvm::Expected<CIAndOrigins> ImportCI = Parse(I, {}, false, false);
+    if (auto E = ImportCI.takeError()) {
+      llvm::errs() << "error: " << llvm::toString(std::move(E)) << "\n";
+      exit(-1);
+    }
+    ImportCIs.push_back(std::move(*ImportCI));
+  }
+
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
 
-  MimimizeFactory CheckFactory;
+  MinimizeActionFactory MinimizeFactory;
   std::unique_ptr<FrontendActionFactory> FrontendFactory;
 
-  FrontendFactory = newFrontendActionFactory(&CheckFactory);
+  FrontendFactory = newFrontendActionFactory(&MinimizeFactory);
 
-  return Tool.run(FrontendFactory.get());
+  int res = Tool.run(FrontendFactory.get());
+  if (res)
+    return res;
+
+  llvm::outs() << MinimizeFactory.getGeneratedEntry();
+
+  return 0;
 }
